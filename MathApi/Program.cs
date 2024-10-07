ad1b23058ea8b8bc5e5003b8b037c62e26dd12f8
AKaurManpreet <156477920+AKaurManpreet@users.noreply.github.com>
2024-10-07 23:02:33 +0000

    registering the CalculatorService

---
+++ MathApi/Program.cs
@@ -0,0 +1,27 @@
+using MathApi.Interfaces;
+using MathApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Register CalculatorService with transient scope
+builder.Services.AddTransient<ICalculatorService, CalculatorService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
